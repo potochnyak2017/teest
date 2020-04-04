36a4f93efc45e6e7fe132f0e0dbaa0a2209205a2
Bohdan Potochniak <bohdan.potochniak@gmail.com>
2020-04-04 23:13:31 +0300

    opu

---
+++ pages/home/home.ts
@@ -0,0 +1,33 @@
+import { Component } from '@angular/core';
+import { NavController } from 'ionic-angular';
+
+@Component({
+  selector: 'page-home',
+  templateUrl: 'home.html'
+})
+export class HomePage {
+
+  constructor(public navCtrl: NavController) {
+
+  }
+lesens = ['Програмування мобільних додатків ', 'Схемотехніка', 'OC'];
+ balls1:number; balls2:number; balls3:number;
+coff1:number; coff2:number; coff3:number;
+result;
+evalRating() {
+this.result=(this.balls1*this.coff1)+(this.balls2*this.coff2)+(this.balls3*this.coff3);
+return this.result;
+}
+
+
+startClick1():void{
+   this.balls1=0;
+   this.coff1=0;
+  };startClick2():void{
+   this.balls2=0;
+   this.coff2=0;
+  };startClick3():void{
+   this.balls3=0;
+   this.coff3=0;
+  };
+}
